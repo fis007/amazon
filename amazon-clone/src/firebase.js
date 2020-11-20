0c4d40e168e121d45aad7481c7e5ab5bdb65e55b
fis007 <schaffhauserfilip@gmail.com>
2020-11-20 13:07:41 +0800

    Finished User Authentication 4:30:27

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDBBXcQpKoWoZIv754haHj5ZD55x9zVlqQ",
   authDomain: "challenge-8e2f1.firebaseapp.com",
@@ -9,3 +11,10 @@ const firebaseConfig = {
   appId: "1:821421095192:web:2141e3cb01c3f176d9066d",
   measurementId: "G-5HMG5LT4KR",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
